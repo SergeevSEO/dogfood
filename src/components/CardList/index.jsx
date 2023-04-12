259e17e6d90b6cb380277c9039ca70f2361cbc26
unknown <sergeevseo@yandex.ru>
2023-04-12 20:18:43 +0300

    final show cards

--- src/components/CardList/index.jsx
+++ src/components/CardList/index.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react"
+import { CardItem } from "../CardItem";
+
 const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDEwN2UwOWFhMzk3MTIxODM4ZjI5MDgiLCJncm91cCI6Imdyb3VwLTExIiwiaWF0IjoxNjc4ODAyNDQ5LCJleHAiOjE3MTAzMzg0NDl9.ueVjcLvvxuzr5_jhp43vMeRe2DSpoJefoUrYrx6zPPQ';
 
 export const CardList = () => {
@@ -11,6 +13,9 @@ export const CardList = () => {
                     Authorization: 'Bearer ' + TOKEN
                 }
             })
+            if(!res.ok){
+                return setProducts({message:"Что-то пошло не так!"})
+            }
             const data = await res.json();
             const products = data.products;
             setProducts(products);
@@ -18,13 +23,17 @@ export const CardList = () => {
         }
         dataProduct();
     }, [])
-
-    console.log(products);   
-
+    
+    if (products.message) {
+        return(
+            <div className="error">{products.message}</div>
+        )
+    }
     return (
+
         <div className="card-list">
             {products.map((product) => {
-                return <div className="card" key={product._id}>{product.name}</div>
+                return <CardItem key={product._id} product = {product} />
             })}
         </div>
         
